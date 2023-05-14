f1ab67e2d5687a7905fdc64eb18707c3385f138c
softdream <1336077399@qq.com>
2023-05-15 00:11:51 +0800

    apf path planner

--- apf_planner/include/apf.h
+++ apf_planner/include/apf.h
@@ -28,21 +28,24 @@ public:
 
 	}
 
-	void setTargetPose( const Vector2& target_pose_ )
+	void setTargetPose( const Vector2& target_pose )
 	{
-		target_pose = target_pose_;
+		target_pose_ = target_pose;
 	}
 
 	// attraction
 	const Vector2 cacuFatt( const Vector2& robot_pose )
 	{
-		DataType dist = distance( robot_pose, target_pose );
-		std::cout<<"dist attraction = "<<dist<<std::endl; 
+		DataType dist = distance( robot_pose, target_pose_ );
 
-		DataType f_att_norm = dist * eta;
+#ifdef DEBUG
+		std::cout<<"dist attraction = "<<dist<<std::endl; 
+#endif
+
+		DataType f_att_norm = dist * eta_;
 	
 		//Vector2 direction = ( target_pose - robot_pose ) / ( target_pose - robot_pose ).norm();
-		Vector2 direction = ( target_pose - robot_pose ).normalized();
+		Vector2 direction = ( target_pose_ - robot_pose ).normalized();
 
 #ifdef DEBUG
 		std::cout<<"attraction direction = "<<direction.transpose()<<std::endl;	
@@ -59,7 +62,7 @@ public:
 			return Vector2::Zero();
 		}
 		
-		DataType rho_g = distance( robot_pose, target_pose );
+		DataType rho_g = distance( robot_pose, target_pose_ );
 
 		// for each obstacle
 		Vector2 f_rep_total = Vector2::Zero();
@@ -73,28 +76,35 @@ public:
 			DataType f_rep1_norm = 0.0;
 			DataType f_rep2_norm = 0.0;
 			
-			if( dist > rho0 ){
+			if( dist > rho0_ ){
 				f_rep1_norm = 0.0;
 				f_rep2_norm = 0.0;
 			}
 			else {
-				f_rep1_norm = cauchy * ( 1 / dist - 1 / rho0 ) * ( ::pow( rho_g, n ) ) / ( ::pow( dist, 2 ) );
-				f_rep2_norm = ( static_cast<DataType>( n ) * cauchy / 2 ) * ( ::pow( ( 1 / dist - 1 / rho0 ), 2 ) ) * ( ::pow( rho_g, n - 1 ) );
+				f_rep1_norm = cauchy_ * ( 1 / dist - 1 / rho0_ ) * ( ::pow( rho_g, n_ ) ) / ( ::pow( dist, 2 ) );
+				f_rep2_norm = ( static_cast<DataType>( n_ ) * cauchy_ / 2 ) * ( ::pow( ( 1 / dist - 1 / rho0_ ), 2 ) ) * ( ::pow( rho_g, n_ - 1 ) );
+
+#ifdef DEBUG
 				std::cout<<"f_rep1_norm "<<i<<" : "<<f_rep1_norm<<std::endl;
+#endif
 			}
 			
 			//Vector2 f_rep1_direction = ( robot_pose - obstacles[i] ) * ( robot_pose - obstacles[i] ).norm();
 			//Vector2 f_rep2_direction = ( target_pose - robot_pose ) * ( target_pose - robot_pose ).norm();
 
 			Vector2 f_rep1_direction = ( robot_pose - obstacles[i] ).normalized();
-			Vector2 f_rep2_direction = ( target_pose - robot_pose ).normalized();
+			Vector2 f_rep2_direction = ( target_pose_ - robot_pose ).normalized();
 
+#ifdef DEBUG
 			std::cout<<"f_rep1 direction "<<i<<" : "<<f_rep1_direction.transpose()<<std::endl<<std::endl;			
+#endif
 
 			// for display
-			repulsion_vec1.push_back( f_rep1_norm * f_rep1_direction );
+			repulsion_vec1_.push_back( f_rep1_norm * f_rep1_direction );
 	
+#ifdef DEBUG
 			std::cout<<"f_rep1 "<<i<<" : "<<( f_rep1_norm * f_rep1_direction ).transpose()<<std::endl;
+#endif
 
 			Vector2 f_rep = ( f_rep1_norm * f_rep1_direction ) + ( f_rep2_norm * f_rep2_direction  );
 
@@ -107,12 +117,12 @@ public:
 
 	const std::vector<Vector2>& getRepulsionVec1() const
 	{
-		return repulsion_vec1;
+		return repulsion_vec1_;
 	}
 
 	void clearRepulsionVec1() 
 	{
-		return repulsion_vec1.clear();
+		return repulsion_vec1_.clear();
 	}
 
 private:
@@ -123,14 +133,14 @@ private:
 
 
 private:
-	DataType eta = 1.0;
-        DataType cauchy = 0.08;
-	DataType rho0 = 0.6;
-	int n = 3;
+	DataType eta_ = 1.0;
+        DataType cauchy_ = 0.08;
+	DataType rho0_ = 0.6;
+	int n_ = 3;
 
-	Vector2 target_pose = Vector2::Zero();
+	Vector2 target_pose_ = Vector2::Zero();
 
-	std::vector<Vector2> repulsion_vec1;
+	std::vector<Vector2> repulsion_vec1_;
 };
 
 }
